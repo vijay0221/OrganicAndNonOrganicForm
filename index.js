031231d046dd79fe3b153c95cc8d79b3142a2251
vijay0221 <vijaymaroju2003@gmail.com>
2023-12-01 16:21:37 +0530

    -

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
  const fs=require('fs');
-
-const http=require('http');
-const url=require('url');
-//////////////////////////////////////////////////
+ 
+ const http=require('http');
+ const url=require('url');
+ const replaceTemplate=require('./modules/replaceTemplate')
+ //////////////////////////////////////////////////
 
 //FILES
 // const textIn=fs.readFileSync('./txt/input.txt','utf-8');
@@ -29,28 +30,10 @@ const url=require('url');
 /////////////////////////////////////////////////////////
 //SERVER
 
-const replaceTemplate=(temp,product)=>{
-  let output=temp.replace(/{%PRODUCTNAME%}/g,product.productName);
-  output=output.replace(/{%IMAGE%}/g,product.image);
-  output=output.replace(/{%PRICE%}/g,product.price);
-  output=output.replace(/{%FROM%}/g,product.from);
-  output=output.replace(/{%NUTRIENTS%}/g,product.nutrients);
-  output=output.replace(/{%QUANTITY%}/g,product.quantity);
-  output=output.replace(/{%DESCRIPTION%}/g,product.description);
-  output=output.replace(/{%ID%}/g,product.id);
-
-  if(!product.organic){
-   output=output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  }
-
-  return output;
-
-}
 
 const tempOverview=fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8')
 const tempCard=fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8')
 const tempProduct=fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8')
-
 const data=fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8')
 const dataObject=JSON.parse(data);
 
@@ -62,14 +45,11 @@ const server=http.createServer((req,res)=>{
   //Overview Page
   if(pathname==='/' || pathname==='/overview'){
     res.writeHead(200,{'Content-type':'text/html'})
-
     const cardsHtml=dataObject.map(el=>replaceTemplate(tempCard,el)).join('')
     const output=tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml)
-    console.log(cardsHtml)
-
     res.end(output) 
 
-    //Product Page
+  //Product Page
   }else if(pathname==='/product'){
    res.writeHead(200,{'Content-type':'text/html'})
     const product=dataObject[query.id]
